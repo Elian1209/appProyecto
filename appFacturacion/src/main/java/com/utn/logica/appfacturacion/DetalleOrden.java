25d793450aa9a00a36800222c20e962e89c45911
Elian1209 <81183582+Elian1209@users.noreply.github.com>
2022-03-07 17:31:10 -0600

    Add files via upload

---
+++ appFacturacion/src/main/java/com/utn/logica/appfacturacion/DetalleOrden.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.utn.logica.appfacturacion;
+
+import java.util.List;
+
+/**
+ *
+ * @author Elian L
+ */
+public class DetalleOrden {
+    private Producto Oproducto;
+    private float precio;
+    private float cantidad;
+    private List<Producto> Pproductos;
+    
+}
